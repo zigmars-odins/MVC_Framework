70aa805b62563ffe3ba1b47ea651e0e9810eab2e
Zigmars Odins <zigmars.odins@gmail.com>
2021-03-26 23:28:21 +0200

    DB work

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -1,60 +1,60 @@
 <?php
 class Database
 {
- private $dbHost = DB_HOST;
- private $dbUser = DB_USER;
- private $dbPass = DB_PASS;
- private $dbName = DB_NAME;
+  private $dbHost = DB_HOST;
+  private $dbUser = DB_USER;
+  private $dbPass = DB_PASS;
+  private $dbName = DB_NAME;
 
- private $statement;
- private $dbHandler;
- private $error;
+  private $statement;
+  private $dbHandler;
+  private $error;
 
- public function __construct()
- {
-  $conn = 'mysqli:host=' . $this->dbHost . ';dbname=' . $this->dbName;
-  $options = array(
-   PDO::ATTR_PERSISTENT => true,
-   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-  );
-  try {
-   $this->dbHandler = new PDO($conn, $this->dbUser, $this->dbPass, $options);
-  } catch (PDOException $e) {
-   $this->error = $e->getMessage();
-   echo $this->error;
+  public function __construct()
+  {
+    $conn = 'mysqli:host=' . $this->dbHost . ';dbname=' . $this->dbName;
+    $options = array(
+      PDO::ATTR_PERSISTENT => true,
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    );
+    try {
+      $this->dbHandler = new PDO($conn, $this->dbUser, $this->dbPass, $options);
+    } catch (PDOException $e) {
+      $this->error = $e->getMessage();
+      echo $this->error;
+    }
   }
- }
- // Allows to write querrys
- public function querry($sql)
- {
-  $this->statement = $this->dbHandler->prepare($sql);
- }
- public function bind($parameter, $value, $type = null)
- {
-  switch (is_null($type)) {
-   case is_int($value):
-    $type = PDO::PARAM_INT;
-    break;
-   case is_bool($value):
-    $type = PDO::PARAM_BOOL;
-    break;
-   case is_null($value):
-    $type = PDO::PARAM_NULL;
-    break;
-   default:
-    $type = PDO::PARAM_STR;
+  // Allows to write querrys
+  public function querry($sql)
+  {
+    $this->statement = $this->dbHandler->prepare($sql);
   }
-  $this->statement->bindValue($parameter, $value, $type);
- }
- // Execute the prepared statement
- public function execute()
- {
-  return $this->statement->execute();
- }
- public function resultSet()
- {
-  $this->execute();
-  return $this->statement->fetchAll(PDO::FETCH_OBJ);
-  // ---------------1:04:02(Complete MVC Framework in OOP)
- }
+  public function bind($parameter, $value, $type = null)
+  {
+    switch (is_null($type)) {
+      case is_int($value):
+        $type = PDO::PARAM_INT;
+        break;
+      case is_bool($value):
+        $type = PDO::PARAM_BOOL;
+        break;
+      case is_null($value):
+        $type = PDO::PARAM_NULL;
+        break;
+      default:
+        $type = PDO::PARAM_STR;
+    }
+    $this->statement->bindValue($parameter, $value, $type);
+  }
+  // Execute the prepared statement
+  public function execute()
+  {
+    return $this->statement->execute();
+  }
+  public function resultSet()
+  {
+    $this->execute();
+    return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    // ---------------1:04:02(Complete MVC Framework in OOP )
+  }
 }
